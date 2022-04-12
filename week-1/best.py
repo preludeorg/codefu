bd62152de7f2ed3851fd4125affbfc4641287a41
Alex <43142871+khyberspache@users.noreply.github.com>
2022-04-12 10:41:08 -0400

    Update best.py

--- week-1/best.py
+++ week-1/best.py
@@ -28,7 +28,6 @@ class BubbleSortConcreteStrategy(SortingStrategy):
         for n in range(len(data) - 1, 0, -1):
             for i in range(n):
                 if data[i] > data[i + 1]:
-                    # swapping data if the element is less than next element in the array
                     data[i], data[i + 1] = data[i + 1], data[i]
         return data
 
