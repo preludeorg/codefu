f2c0fc1460528d0cb1623b122c84168ad1a9a952
Alex <43142871+khyberspache@users.noreply.github.com>
2022-04-12 10:40:46 -0400

    Update bad.py

--- week-1/bad.py
+++ week-1/bad.py
@@ -22,7 +22,6 @@ class Bad:
         for n in range(len(data) - 1, 0, -1):
             for i in range(n):
                 if data[i] > data[i + 1]:
-                    # swapping data if the element is less than next element in the array
                     data[i], data[i + 1] = data[i + 1], data[i]
         return data
 
